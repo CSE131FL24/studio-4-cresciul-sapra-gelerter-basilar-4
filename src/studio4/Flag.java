e8e90ac9a39767905bc0a00110e6746486d60697
andrewcresciullo <andrewcresciullo@localhost>
2024-10-03 11:12:39 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,29 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		Color LightPink = new Color(255, 102, 102);
+		double width = 0.5;
+		double halfwidth = 0.5;
+		double height = 0.25;
+		double halfheight = 0.125;
+		
+		StdDraw.setPenColor(LightPink);
+		StdDraw.filledRectangle( width , halfwidth , height , halfheight);
+		
+		
+		StdDraw.setPenColor(Color.blue);
+		double x = 0.5;
+		StdDraw.filledCircle (x, x , x/5);
+		Color LightBlue = new Color(51, 204, 255);
+		StdDraw.setPenColor(LightBlue);
+		StdDraw.filledCircle (x, x , x/7);
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.filledCircle (x, x , x/10);
+		Color white = new Color(255, 255, 255);
+		StdDraw.setPenColor(white);
+		StdDraw.filledCircle (x, x , x/12);
+		StdDraw.setPenColor(Color.blue);
+		StdDraw.filledCircle (x, x , x/15);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -14,11 +15,41 @@ import edu.princeton.cs.introcs.StdDraw;
  */
 public class InterpretDrawingFile {
 
+	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		
+		String Shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		
+		
+		Color Flag = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(Flag);
+		boolean Isfilled = in.nextBoolean();
+		
+		
+	
+		System.out.print(Isfilled);
+		
+		if (Shape.equals("Rectange")){
+			
+			Color Pen = new Color(redComponent, greenComponent, blueComponent);
+			double ParameterOne = in.nextDouble();
+			double ParameterTwo = in.nextDouble();
+			double ParameterThree = in.nextDouble();
+			double ParameterFour = in.nextDouble();
+			StdDraw.filledRectangle(ParameterOne, ParameterTwo, ParameterThree, ParameterFour);
+		}
+		
+		
+		
 		
 	}
 }
